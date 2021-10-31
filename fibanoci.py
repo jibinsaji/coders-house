11232a75eb0776be87f75d4ed9741873b6da59f5
jibinsaji <91717097+jibinsaji@users.noreply.github.com>
2021-10-31 10:49:09 +0530

    Create fibanoci.py

---
+++ fibanoci.py
@@ -0,0 +1,16 @@
+n=int(input("enter the num"))
+i=2
+n1=0
+n2=1
+if n==1:
+    print(n1)
+elif n==2:
+    print(n1,n2)
+else:
+    print(n1,n2,end="")
+    while i<n:
+        n3=n1+n2
+        print("",n3,end="")
+        n1=n2
+        n2=n3
+        i=i+1
